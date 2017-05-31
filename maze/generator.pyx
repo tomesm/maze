e0e3b16af9dc896ef3334600bcdf2ea7132e9d26
Martin Tomes <tomesm@gmail.com>
2017-05-31 12:54:08 +0200

    Finish cython generator and add new tests

--- maze/generator.pyx
+++ maze/generator.pyx
@@ -1,5 +1,4 @@
 import numpy
-#rom numpy.random import random_integers as rand
 
 cimport numpy
 cimport cython
@@ -16,24 +15,9 @@ cdef extern from "time.h":
 cdef int randint(int limit):
     return rand() % limit
 
-def generate_maze(int width=81, int height=51, double complexity=0.75, double density=0.75):
-    """ Convert the maze to required format """
-    # We ned walls marked as negative numbers so we need to re-type the base
-    maze = generate_base_maze(width, height, complexity, density).astype(int)
-    # rewrite walls as -1
-    maze[maze == 1] = -1
-    # non-negatives (zeros in this case) are cells so no need to customize
-    # set target at random cell
-    cells = numpy.where(maze == 0)
-    target = numpy.random.randint(0, len(cells[0]))
-    # set target coordinates inside sells arrays
-    target_x = cells[0][target]
-    target_y = cells[1][target]
-    maze[target_x, target_y] = 1
-    return maze
 
-
-cpdef numpy.ndarray[numpy.int8_t, ndim=2] generate_base_maze(int width=81, int height=51, double complexity=0.75, double density=0.75):
+cpdef numpy.ndarray[numpy.int8_t, ndim=2] generate_maze(int width=81,int height=51,
+                                                    double complexity=0.75, double density=0.75):
     """ Let's just re-use a generator from Wikipeida:
 
             Source: https://en.wikipedia.org/wiki/Maze_generation_algorithm
@@ -47,29 +31,39 @@ cpdef numpy.ndarray[numpy.int8_t, ndim=2] generate_base_maze(int width=81, int h
     cdef int icomplexity = int(complexity * (5 * (shape[0] + shape[1])))
     cdef int idensity = int(density * ((shape[0] // 2) * (shape[1] // 2)))
     # Build actual maze
-    cdef numpy.ndarray[numpy.int8_t, ndim=2] Z = numpy.zeros(shape, dtype=bool)
+    cdef numpy.ndarray[numpy.int8_t, ndim=2] Z = numpy.zeros(shape, dtype=numpy.int8)
     # Fill borders
     Z[0, :] = Z[-1, :] = 1
     Z[:, 0] = Z[:, -1] = 1
     # Make aisles
+    cdef int i, j, x, y, x_, y_, idx
+    cdef numpy.ndarray[numpy.int_t, ndim=2] neighbours = numpy.empty((4, 2), dtype=numpy.int)
     for i in range(idensity):
-        x, y = randint(shape[1] // 2) * 2, randint(shape[0] // 2) * 2
-        Z[y, x] = 1
+        x, y = randint(shape[1] // 2 + 1) * 2, randint(shape[0] // 2 + 1) * 2
+        Z[y, x] = -1
         for j in range(icomplexity):
-            neighbours = []
+            idx = 0
             if x > 1:
-                neighbours.append((y, x - 2))
+                neighbours[idx, 0] = y
+                neighbours[idx, 1] = x - 2
+                idx += 1
             if x < shape[1] - 2:
-                neighbours.append((y, x + 2))
+                neighbours[idx, 0] = y
+                neighbours[idx, 1] = x + 2
+                idx += 1
             if y > 1:
-                neighbours.append((y - 2, x))
+                neighbours[idx, 0] = y - 2
+                neighbours[idx, 1] = x
+                idx += 1
             if y < shape[0] - 2:
-                neighbours.append((y + 2, x))
-            if len(neighbours):
-                y_, x_ = neighbours[randint(len(neighbours) - 1)]
+                neighbours[idx, 0] = y + 2
+                neighbours[idx, 1] = x
+                idx += 1
+            if idx:
+                y_, x_ = neighbours[randint(idx), 0], neighbours[randint(idx), 1]
                 if Z[y_, x_] == 0:
-                    Z[y_, x_] = 1
-                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                    Z[y_, x_] = -1
+                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = -1
                     x, y = x_, y_
     return Z
 
